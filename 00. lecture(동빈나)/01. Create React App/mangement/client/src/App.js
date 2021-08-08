d089d5e8384184f7060dafc8aaf2534bd394baf2
이혜은 <snflo16@naver.com>
2021-08-08 23:22:43 +0900

    Add Get Customer REST API Module

--- 00. lecture(동빈나)/01. Create React App/mangement/client/src/App.js
+++ 00. lecture(동빈나)/01. Create React App/mangement/client/src/App.js
@@ -12,42 +12,32 @@ import { withStyles } from '@material-ui/core';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto",
   },
   table: {
     minWidth: 1080,
   }
-})
-
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '이혜은1',
-    'birthday': '9806121',
-    'gender': '여자1',
-    'job': '백수1'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '이혜은2',
-    'birthday': '9806122',
-    'gender': '여자2',
-    'job': '백수2'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이혜은3',
-    'birthday': '9806123',
-    'gender': '여자3',
-    'job': '백수3'
-  },
-]
+});
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err))
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return vody
+  }
+
   // function App() {
   render() {
     const { classes } = this.props;
@@ -65,7 +55,9 @@ class App extends Component {
             </TableRow>  
           </TableHead>
           <TableBody>
-            {customers.map(c => { return ( <Customer key={c.id} id={c.id} img={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+            {this.state.customers ? this.state.customers.map(c => { 
+              return ( <Customer key={c.id} id={c.id} img={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- 00. lecture(동빈나)/01. Create React App/mangement/server.js
+++ 00. lecture(동빈나)/01. Create React App/mangement/server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '이혜은1',
+            'birthday': '9806121',
+            'gender': '여자1',
+            'job': '백수1'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '이혜은2',
+            'birthday': '9806122',
+            'gender': '여자2',
+            'job': '백수2'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '이혜은3',
+            'birthday': '9806123',
+            'gender': '여자3',
+            'job': '백수3'
+        },
+    ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
