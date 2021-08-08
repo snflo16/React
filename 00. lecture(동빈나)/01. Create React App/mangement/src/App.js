e1b0c2971f9397f8745fdf4f85c3e96b6aed8cea
이혜은 <snflo16@naver.com>
2021-08-08 14:50:19 +0900

    Create Customer.js

--- 00. lecture(동빈나)/01. Create React App/mangement/src/App.js
+++ 00. lecture(동빈나)/01. Create React App/mangement/src/App.js
@@ -1,14 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from 'react';
+import Customer from './components/Customer'
+import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '이혜은1',
+    'birthday': '9806121',
+    'gender': '여자1',
+    'job': '백수1'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '이혜은2',
+    'birthday': '9806122',
+    'gender': '여자2',
+    'job': '백수2'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이혜은3',
+    'birthday': '9806123',
+    'gender': '여자3',
+    'job': '백수3'
+  },
+]
 
 class App extends Component {
   // function App() {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop mamagement system!</h2>
+      <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              img={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+            />
+          )
+        })
+      }
       </div>
     );
   }
